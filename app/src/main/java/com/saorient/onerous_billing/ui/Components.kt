2edbbc0c9cee33e3adf1c66be7c32bf2bb57680f
= <=>
2023-03-03 09:51:22 +0300

    add outlined text input component

--- app/src/main/java/com/saorient/onerous_billing/ui/Components.kt
+++ app/src/main/java/com/saorient/onerous_billing/ui/Components.kt
@@ -1,8 +1,35 @@
 package com.saorient.onerous_billing.ui
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import com.saorient.onerous_billing.ui.composables.LoginScreen
+import com.saorient.onerous_billing.ui.theme.Onerous_BillingTheme
 
 @Composable
-fun illustration() {
+fun textEdit(modifier:Modifier= Modifier, label:String="") {
+    var text by remember{mutableStateOf("")}
+    OutlinedTextField(value = text, onValueChange = {
+        text=it
+    }, label={Text(label)},modifier= modifier)
 
+
+
+}
+
+@Composable
+fun button(modifier: Modifier= Modifier, ) {
+    
+}
+@Preview(showBackground = true)
+@Composable
+fun textEditPreview() {
+    Onerous_BillingTheme {
+       textEdit( modifier = Modifier.background(MaterialTheme.colorScheme.background),label = "password")
+    }
 }
